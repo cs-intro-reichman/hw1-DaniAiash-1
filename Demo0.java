8ca8ae0373426646d83f20200325f398af4901b7
DaniAiash <Daniaiash77@gmail.com>
2025-11-05 14:38:14 +0200

    finished bill3

--- Demo0.java
+++ Demo0.java
@@ -3,7 +3,7 @@
 public class Demo0 {
 	public static void main(String[] args) {
 		// Declares an int variable, and sets it to 1
-        for (int i = 0; i <= 5; i++) {
+        for (int i = 0; i <= 10; i++) {
             System.out.println(i);
 
         }
